e1c8826f4b0ccab7ae22697e24b3b9f1a59f5a25
Nathan <157889418+natty-mck@users.noreply.github.com>
2025-02-06 19:16:59 +0000

    Add files via upload

---
+++ script.js
@@ -0,0 +1,141 @@
+var currentpage = 1;
+document.body.style.overflow = "hidden";
+//greeting text typing code
+async function greeting() {
+    document.getElementById("mainSection").scrollIntoView({ behavior: 'smooth' });
+    let phrase = "/Welcome...";
+    let greeting = document.getElementById("welcomeText");
+    let i = 0;
+
+    function type() {
+        return new Promise((resolve) => {
+            if (i < phrase.length) {
+                greeting.innerHTML += phrase[i];
+                i++;
+                setTimeout(() => {
+                    type().then(resolve);
+                }, Math.floor(Math.random() * 200) + 1);
+            }
+            else {
+                resolve();
+            }
+        })
+    }
+    
+    function typeAboutMe() {
+        return new Promise((resolve) => {
+            if (i < phrase.length) {
+                if (phrase[i-1] == ">") {
+                    greeting.innerHTML += '<br>';
+                    greeting.style.borderRightWidth = "0px";
+                    greeting = document.getElementById("welcomePar");
+                }
+                greeting.innerHTML += phrase[i];
+
+                i++;
+                setTimeout(() => {
+                    typeAboutMe().then(resolve);
+                }, Math.floor(Math.random() * 200) + 1);
+            }
+            else {
+                resolve();
+            }
+        })
+    }
+    
+    
+    await type();
+    i= 0;
+    phrase = "<br> Hello, I am a west suffolk college student.";
+    await typeAboutMe()
+}
+
+console.log("Everything is in it's right place.")
+greeting()
+
+
+// changing pages
+document.addEventListener('wheel', function(event) {
+    if (event.deltaY > 0) {
+        nextpage()
+    }
+});
+
+document.addEventListener('wheel', function(event) {
+    if (event.deltaY < 0) {
+        lastpage()
+    }
+});
+
+async function nextpage() {
+    document.body.style.overflow = "auto";
+    function goToNextSection() {
+        return new Promise((resolve) => {
+            switch (currentpage) {
+            case 1:
+                document.getElementById("projectsPage-mainSection").scrollIntoView({ behavior: 'smooth' });
+                document.getElementById("home-icon").src = "assets/home-icon.png"
+                document.getElementById("projects-icon").src = "assets/project-icon selected.png"
+                currentpage = 2;
+                break;
+            case 2:
+                document.getElementById("contactPage-mainSection").scrollIntoView({ behavior: 'smooth' });
+                document.getElementById("projects-icon").src = "assets/project-icon.png";
+                document.getElementById("contact-icon").src = "assets/contact-icon selected.png";
+                currentpage = 3;
+                break;
+            }
+            resolve()
+        })
+    }
+
+    await goToNextSection() 
+    document.body.style.overflow = "hidden";
+}
+
+async function lastpage() {
+    document.body.style.overflow = "auto";
+    function goToNextSection() {
+        return new Promise((resolve) => {
+            switch (currentpage) {
+            case 2:
+                document.getElementById("mainSection").scrollIntoView({ behavior: 'smooth' });
+                document.getElementById("home-icon").src = "assets/home-icon selected.png"
+                document.getElementById("projects-icon").src = "assets/project-icon.png"
+                currentpage = 1;
+                break;
+            case 3:
+                document.getElementById("projectsPage-mainSection").scrollIntoView({ behavior: 'smooth' });
+                document.getElementById("projects-icon").src = "assets/project-icon selected.png"
+                document.getElementById("contact-icon").src = "assets/contact-icon.png";
+                currentpage = 2;
+            break;            }
+            resolve()
+        })
+    }
+
+    await goToNextSection() 
+    document.body.style.overflow = "hidden";
+}
+
+function toHomepage() {
+    document.getElementById("mainSection").scrollIntoView({ behavior: 'smooth' });
+    document.getElementById("home-icon").src = "assets/home-icon selected.png";
+    document.getElementById("projects-icon").src = "assets/project-icon.png";
+    document.getElementById("contact-icon").src = "assets/contact-icon.png";
+    currentpage = 1;
+}
+function toProjectspage() {
+    document.getElementById("projectsPage-mainSection").scrollIntoView({ behavior: 'smooth' });
+    document.getElementById("projects-icon").src = "assets/project-icon selected.png"
+    document.getElementById("contact-icon").src = "assets/contact-icon.png";
+    document.getElementById("home-icon").src = "assets/home-icon.png";
+    currentpage = 2;
+}
+function toContactPage() {
+    document.getElementById("contactPage-mainSection").scrollIntoView({ behavior: 'smooth' });
+    document.getElementById("projects-icon").src = "assets/project-icon.png";
+    document.getElementById("home-icon").src = "assets/home-icon.png";
+    document.getElementById("contact-icon").src = "assets/contact-icon selected.png";
+    currentpage = 3;
+}
